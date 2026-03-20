b2bbb97d48525f9a274d65c836b890c53706a99a
cibele <cibele.as10@gmail.com>
2026-03-20 17:50:31 +0100

    Fix Format Code

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,32 +1,27 @@
-import LoginPage from '../../pages/loginPage'
+import LoginPage from "../../pages/loginPage";
 
-const login = new LoginPage()
+const login = new LoginPage();
 
-describe('RealWorld App', () => {
-    it('Login Successfully', () => {
-      // adjust the pattern to match your app's login endpoint (inspect in DevTools Network)
-         cy.intercept('POST', '**/login').as('loginRequest')
+describe("RealWorld App", () => {
+  it("Login Successfully", () => {
+    // adjust the pattern to match your app's login endpoint (inspect in DevTools Network)
+    cy.intercept("POST", "**/login").as("loginRequest");
 
-        login.accessLoginPage()
-        login.loginWithCredentials('admin01', 'test1234')
+    login.accessLoginPage();
+    login.loginWithCredentials("admin01", "test1234");
 
-        // wait for dashboard to load after login
-      //  cy.wait('@loginRequest').its('response.statusCode').should('eq',200)
+    // wait for dashboard to load after login
+    //  cy.wait('@loginRequest').its('response.statusCode').should('eq',200)
+  });
 
-    })
+  it("Login Unsuccessfully", () => {
+    login.accessLoginPage();
+    login.loginWithCredentials("admin", "1234");
+    login.checkWrongCredentialsError();
+  });
 
-
-    it('Login Unsuccessfully', () => {
-        login.accessLoginPage()
-        login.loginWithCredentials('admin', '1234')
-        login.checkWrongCredentialsError()     
-
-    })
-
-    it ('Login with empty fields', () => {
-        login.accessLoginPage()
-        login.checkEmptyFieldsError() 
-        
-        
-    })
- })
+  it("Login with empty fields", () => {
+    login.accessLoginPage();
+    login.checkEmptyFieldsError();
+  });
+});
--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,44 +1,37 @@
-
-  class LoginPage {
-
+class LoginPage {
   selectorsList() {
-
-  const selectors = {
+    const selectors = {
       usernameField: '[name="username"]',
       passwordField: '[name="password"]',
       loginButton: '[data-test="signin-submit"]',
       wrongCredentialsError: '[data-test="signin-error"]',
-      emptyFieldError: 'input[aria-invalid="true"]'
+      emptyFieldError: 'input[aria-invalid="true"]',
+    };
+
+    return selectors;
   }
 
-  return selectors
-}
-
- accessLoginPage () {
-    cy.visit('http://localhost:3000/signin')
-    
- }
-
- //Check login success credentials
-
- loginWithCredentials(username,password) {
-  cy.get(this.selectorsList().usernameField).type(username)
-  cy.get(this.selectorsList().passwordField).type(password)
-  cy.get(this.selectorsList().loginButton).click()
-  
-
- }
-// Check wrong credentials error
- checkWrongCredentialsError() {
-  cy.get(this.selectorsList().wrongCredentialsError).should('be.visible')
- }
-
- //Check empty fields error
- checkEmptyFieldsError() {
-  cy.get(this.selectorsList().loginButton).click()
-  cy.get(this.selectorsList().emptyFieldError).should('be.visible')
+  accessLoginPage() {
+    cy.visit("http://localhost:3000/signin");
   }
- 
+
+  //Check login success credentials
+
+  loginWithCredentials(username, password) {
+    cy.get(this.selectorsList().usernameField).type(username);
+    cy.get(this.selectorsList().passwordField).type(password);
+    cy.get(this.selectorsList().loginButton).click();
+  }
+  // Check wrong credentials error
+  checkWrongCredentialsError() {
+    cy.get(this.selectorsList().wrongCredentialsError).should("be.visible");
+  }
+
+  //Check empty fields error
+  checkEmptyFieldsError() {
+    cy.get(this.selectorsList().loginButton).click();
+    cy.get(this.selectorsList().emptyFieldError).should("be.visible");
+  }
 }
 
-export default LoginPage // export default LoginpAGE
+export default LoginPage; // export default LoginpAGE
